913c9e602232011e82f295d142b199430e2d6bdb
Rodamanthosch <rodch51@gmail.com>
2026-05-14 19:52:17 +0300

    Fix: remove eager imports from __init__.py files

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,1 @@
-from .train import Denoiser
-from .utils import (
-    load_config, save_checkpoint, load_checkpoint,
-    paper_peak_lr, make_lr_fn,
-    evaluate_fid_is, measure_throughput, MetricLogger,
-)
+# Import directly from submodules, e.g: from src.models.jit import JiT
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,1 @@
-from .jit import JiT
-from .vim import JiTViM
-from .vmamba import JiTVMamba
+# Import directly: from src.models.jit import JiT
